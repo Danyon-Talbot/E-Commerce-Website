5c832a2d44ea89f67194b2c7aa300e2081155757
danyon talbot <danyon.talbot2@gmail.com>
2023-11-20 16:05:49 +1100

    Adding References for Columns

--- models/Product.js
+++ models/Product.js
@@ -24,15 +24,26 @@ Product.init(
     price: {
       type: DataTypes.DECIMAL,
       allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
     },
     stock: {
       type: DataTypes.INTEGER,
-      allowNull: true,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
     },
     category_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
-    },
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    }, 
   },
   {
     sequelize,
