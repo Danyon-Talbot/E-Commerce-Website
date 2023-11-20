94e75feb891b4f81df3c162d4a2a6c08f3a6e74f
danyon talbot <danyon.talbot2@gmail.com>
2023-11-20 20:32:48 +1100

    Updated Add Product Route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -38,36 +38,45 @@ router.get('/:id', async (req, res) => {
 
 
 // create new product
-router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-  Product.create(req.body)
-    .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
+router.post('/', async (req, res) => {
+  try {
+    const { product_name, price, stock, tagIds, category_id } = req.body;
+
+    // First, create the product
+    const newProduct = await Product.create({
+      product_name,
+      price,
+      stock,
+      category_id, // Associate the product with the specified category
     });
+
+    // Next, associate the product with tags (if any)
+    if (tagIds && tagIds.length > 0) {
+      const productTagIdArr = tagIds.map((tag_id) => {
+        return {
+          product_id: newProduct.id,
+          tag_id,
+        };
+      });
+      await ProductTag.bulkCreate(productTagIdArr);
+    }
+
+    // Fetch the created product along with its associated category and tags
+    const createdProduct = await Product.findByPk(newProduct.id, {
+      include: [
+        { model: Category },
+        { model: Tag, through: ProductTag },
+      ],
+    });
+
+    res.status(200).json(createdProduct);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json(err);
+  }
 });
+
+
 
 // update product
 router.put('/:id', (req, res) => {
