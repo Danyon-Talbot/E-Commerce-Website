ebf0426b05b0a7f89ab09e85c28b47d82b3fa5cd
danyon talbot <danyon.talbot2@gmail.com>
2023-11-20 20:11:46 +1100

    Added More Routes to Category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -34,13 +34,42 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  // create new category
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(200).json(newCategory);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryId = req.params.id;
+    const updatedCategory = await Category.update(req.body, {
+      where: {
+        id: categoryId,
+      },
+    });
+
+    if (updatedCategory[0] === 0) {
+      // If no category was updated, respond with a 404 status and a message
+      res.status(404).json({ message: 'Category not found' });
+    } else {
+      // If the category was successfully updated, respond with a 200 status
+      res.status(200).json({ message: 'Category updated successfully' });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
+;
+
+
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
